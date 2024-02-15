2730c4f643005bfb2081c86f7ee9d5fe2eebe9cd
Ömer Kolsuz <112700914+omerkolsuz@users.noreply.github.com>
2024-02-15 14:10:04 +0100

    Add files via upload

---
+++ Inbyggda system/Labb6/lab6/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+delay\delay_asm.s
+
+hmi\hmi.c
+
+lab6.c
+
+lcd\lcd.c
+
+motor\motor.c
+
+numkey\numkey.c
+
+regulator\regulator.c
+
---
+++ Inbyggda system/Labb6/lab6/common.h
@@ -0,0 +1,44 @@
+﻿/*
+ * common.h
+ *
+ * Author:	Ibrahim Akiel och Ömer Kolsuz
+ *
+ * Date:	2022-12-06
+
+The purpose of this file is to set specific bits for the specified register
+
+ */ 
+
+
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+/*
+ * This macro is used to set a specific bit in a register.
+ */
+//#define SET_BIT(reg, pos)	(reg) = (reg)
+#define SET_BIT(reg, pos) (reg |= (1 << pos))
+
+
+
+
+
+
+/*
+ * This macro is used to clear a specific bit in a register.
+ */
+#define CLR_BIT(reg, pos)    (reg &= ~(1 << pos))
+
+/*
+ * This macro is used to modify several bits of a register.
+ * Example:
+ *	Bit 7-4 of PORTD should be set to 1010, while the
+ *	rest of the register should NOT be modified! Usage:
+ *		SET_BIT_LEVELS(PORTD, 0b00001111, 0b10100000);
+ * The bit mask is used to clear the bits that should be modified.
+ */
+#define SET_BIT_LEVELS(reg, bit_mask, bit_data) \
+(reg) = (((reg) & (bit_mask)) | ((bit_data) & ~(bit_mask)))
+
+#endif /* COMMON_H_ */
